5e2e9ba259ad9bda4b40a4b8fca515e82208922b
manith mathews <mathewsmanith@gmail.com>
2023-09-10 21:13:21 +1000

    First time commit

---
+++ AugSelenium2023/src/pkg1/TestCase01.java
@@ -0,0 +1,21 @@
+package pkg1;
+
+
+
+public class TestCase01 {
+
+	static int x=100;
+	int b=200;
+	
+	public static void main(String[] args) {
+		
+		TestCase01.x=200;
+		System.out.println(TestCase01.x);
+		
+		TestCase01 obj1=new TestCase01();
+		System.out.println(obj1.b); 
+		
+		
+		
+	}
+}
